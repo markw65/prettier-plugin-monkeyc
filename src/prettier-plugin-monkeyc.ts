85661218cd92aeefe075396997db225efa330847
markw65 <mark_st@myosotissp.com>
2022-06-09 08:33:21 -0700

    Make monkeyc-json parser smarter about comments

--- src/prettier-plugin-monkeyc.ts
+++ src/prettier-plugin-monkeyc.ts
@@ -34,13 +34,32 @@ export const parsers = {
     // original text, followed by the json ast. This is because
     // prettier makes some of its formatting decisions by looking
     // at the original source, rather than the contents of the ast.
-    parse: (str: string): ESTreeProgram =>
-      JSON.parse(
-        str.replace(
-          /^(.|[\r\n\u2028\u2029])*(\r\n|[\n\r\u2028\u2029])(.)/,
-          "$3"
-        )
-      ),
+    parse: (str: string): ESTreeNode => {
+      const match = str.match(
+        /^((.|[\r\n\u2028\u2029])*)(\r\n|[\n\r\u2028\u2029])(.+)$/
+      );
+      const result = JSON.parse(match ? match[4] : str) as ESTreeNode;
+      if ("comments" in result) {
+        if (
+          !match ||
+          !match[1] ||
+          (result.end && match[1].length < result.end)
+        ) {
+          // If we didn't get original text, don't try to print comments
+          // because prettier ignores the comment value, and reads the
+          // text of the comment from the source.
+          delete result.comments;
+        } else if (result.comments) {
+          // And if we did get the original text, ignore any
+          // prettier-ignore directives, because that would
+          // throw away any changes we made to the ast.
+          result.comments = result.comments.filter(
+            (comment) => !comment.value.includes("prettier-ignore")
+          );
+        }
+      }
+      return result;
+    },
     astFormat: "monkeyc",
     locStart: (node) => node.start || 0,
     locEnd: (node) => node.end || 0,
