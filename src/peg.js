eb918d5feb3f44044dbfb8e7b2494b990236c99f
markw65 <mark_st@myosotissp.com>
2022-11-30 10:07:16 -0800

    More options, and add timing info to peg.js

--- src/peg.js
+++ src/peg.js
@@ -1,29 +1,57 @@
 import * as fs from "fs/promises";
 import { argv } from "process";
-import * as parse from "../build/monkeyc.js";
+import {
+  default as MonkeyC,
+  serializeMonkeyC,
+} from "../build/prettier-plugin-monkeyc.cjs";
 
 if (process.argv.length <= 2) {
   console.error("No files to process!");
   process.exit(1);
 }
 
-Promise.all(
-  argv.slice(2).map((file) =>
-    fs
-      .readFile(file)
-      .then((data) => {
-        return {
-          file,
-          ast: parse.parse(data.toString()),
-        };
-      })
-      .catch((data) => {
-        let message = data.toString();
-        if (data.location) {
-          message += `in file '${file}' at line ${data.location.start.line}, column ${data.location.start.column}`;
-        }
-        console.error(message);
-        process.exit(1);
-      })
+const args = argv.slice(2);
+let time = false;
+if (args[0] === "--time") {
+  args.splice(0, 1);
+  time = true;
+}
+let total = 0;
+const start = Date.now();
+const results = [`${new Date().toLocaleString()} - Start`];
+args
+  .reduce(
+    (promise, file) =>
+      promise.then(() => {
+        const s = Date.now();
+        return fs
+          .readFile(file)
+          .then((data) => {
+            const ast = MonkeyC.parsers.monkeyc.parse(data.toString(), null, {
+              filepath: file,
+            });
+            const duration = Date.now() - s;
+            total += duration;
+            results.push({
+              file,
+              data: time ? duration : ast,
+            });
+          })
+          .catch((data) => {
+            let message = data.toString();
+            if (data.location) {
+              message += `in file '${file}' at line ${data.location.start.line}, column ${data.location.start.column}`;
+            }
+            results.push({ file, data: message });
+          });
+      }),
+    Promise.resolve()
   )
-).then((results) => console.log(JSON.stringify(results)));
+  .then(() => {
+    results.push(
+      `${new Date().toLocaleString()} - Done in ${
+        Date.now() - start
+      }ms (parsing took ${total}ms)`
+    );
+    console.log(serializeMonkeyC(results));
+  });
